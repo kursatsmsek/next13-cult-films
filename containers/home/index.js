3f6cd89cff64a7181c2adabe91f3b7907a218c8b
kursatsmsek <kursatsimsek@protonmail.ch>
2023-03-22 01:08:48 +0300

    movies section added

--- containers/home/index.js
+++ containers/home/index.js
@@ -3,12 +3,21 @@ import Categories from "@/components/categories"
 import React from "react"
 import Movies from "@/mocks/movies.json"
 import Genres from "@/mocks/genres.json"
+import { MoviesSection } from "@/components/movies-section"
 
 function HomeContainer() {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]} />
       <Categories categories={Genres.genres.slice(0, 5)} />
+      <MoviesSection
+        title="Popular Films"
+        movies={Movies.results.slice(1, 7)}
+      />
+      <MoviesSection
+        title="Your Favorites"
+        movies={Movies.results.slice(7, 13)}
+      />
     </div>
   )
 }
